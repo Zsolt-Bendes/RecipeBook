70c7157ee74ce0ab438e6eeab970eff7bd3cf45e
Zsolt Bendes <zsolt.bendes@gmail.com>
2024-12-26 11:14:36 +0100

    add remove ingredient endpoint, add tests for remove ingredient

---
+++ src/Home.Recipes.Api/Features/Recipes/RemoveIngredient.cs
@@ -0,0 +1,43 @@
+﻿using Home.Recipes.Domain.Recipes;
+using Home.Recipes.Domain.Recipes.Events;
+using Marten;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using Wolverine.Http;
+using Wolverine.Marten;
+
+namespace Home.Recipes.Api.Features.Recipes;
+
+public sealed record RemoveIngredientCommand(Guid RecipeId, int Index);
+
+public static class RemoveIngredientEndpoint
+{
+    internal const string Endpoint = "/recipes/removeIngredient";
+
+    public static async Task<ProblemDetails> LoadAsync(
+       RemoveIngredientCommand command,
+       IDocumentSession session,
+       CancellationToken cancellationToken)
+    {
+        var recipe = await session.LoadAsync<Recipe>(command.RecipeId, cancellationToken);
+        if (recipe is null)
+        {
+            return
+                new ProblemDetails
+                {
+                    Title = "Recipe not found",
+                    Status = (int)HttpStatusCode.NotFound,
+                    Detail = $"Recipe with id {command.RecipeId} not found",
+                };
+        }
+
+        return WolverineContinue.NoProblems;
+    }
+
+    [WolverinePut(Endpoint)]
+    [AggregateHandler, EmptyResponse]
+    public static IngredientRemoved Put(RemoveIngredientCommand command, Recipe recipe)
+    {
+        return new IngredientRemoved(command.Index);
+    }
+}
---
+++ test/Home.Recipes.IntegrationTests/Recipes/When_removing_ingredient.cs
@@ -0,0 +1,59 @@
+﻿using Home.Recipes.Api.Features.Recipes;
+using System.Net;
+
+namespace Home.Recipes.IntegrationTests.Recipes;
+
+[Collection(nameof(Fixture))]
+public sealed class When_removing_ingredient
+{
+    private readonly Fixture _fixture;
+
+    public When_removing_ingredient(Fixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact]
+    public async Task Given_recipe_does_not_exists_then_not_found_is_returned()
+    {
+        // Arrange
+        var dto = new RemoveIngredientCommand(Guid.NewGuid(), 1);
+
+        // Act & Assert
+        _ = await _fixture.Host.Scenario(_ =>
+        {
+            _.Put.Url(RemoveIngredientEndpoint.Endpoint);
+            _.Put.Json(dto);
+            _.StatusCodeShouldBe(HttpStatusCode.NotFound);
+        });
+    }
+
+    [Fact]
+    public async Task Given_recipe_exists_with_ingredient_then_ingredient_is_removed_from_recipe()
+    {
+        // Arrange
+        var evt = ObjectMother
+           .Events
+           .RecipeCreated
+           .Default
+           .WithPieceIngredients(_ => _.Build())
+           .Build();
+        await _fixture.CreateRecipeAsync(evt);
+
+        var dto = new RemoveIngredientCommand(evt.RecipeId, 0);
+
+        // Act
+        _ = await _fixture.Host.Scenario(_ =>
+        {
+            _.Put.Url(RemoveIngredientEndpoint.Endpoint);
+            _.Put.Json(dto);
+            _.StatusCodeShouldBe(HttpStatusCode.NoContent);
+        });
+
+        // Assert
+        var recipeFromDb = await _fixture.LoadRecipeFromDbAsync(evt.RecipeId);
+
+        recipeFromDb.Should().NotBeNull();
+        recipeFromDb!.Ingredients.Should().HaveCount(0);
+    }
+}

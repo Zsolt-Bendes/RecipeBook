eaa25ebf2c6d123b5408ea8b8a6b2f4c9eec491c
Zsolt Bendes <zsolt.bendes@gmail.com>
2025-01-08 20:26:42 +0100

    add base component for buttons (for css class inheritence)

---
+++ src/Home.Recipes.WebClient/Components/CompBase.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Home.Recipes.WebClient.Components;
+
+public abstract class CompBase : ComponentBase
+{
+    [Parameter]
+    public string Class { get; set; } = string.Empty;
+}
